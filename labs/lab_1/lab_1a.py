baf39993dbc00eaf21091fb859a59bce4dde488e
danialdol <118039230+danialdol@users.noreply.github.com>
2026-03-25 19:48:56 -0400

    introduction to danialdo

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,10 @@ def main():
     name = "danialdo luna" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print('HELLOOOOO my name is Danialdo Luna im currently a junior'
+    'in highschool, im interested in video games like many other'
+    'people but in my free time i try giving back to my'
+    'community by hosting events within my school or various programs '
+    'im in. ')
 if __name__ == "__main__":
     main()
